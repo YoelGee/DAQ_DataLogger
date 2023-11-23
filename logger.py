d4b4a39cfe017ab1473f9d111d71107c672d1557
Yoel Ghebrecristos <yghebrec@uci.edu>
2023-11-22 19:54:32 -0800

    added data logger

--- logger.py
+++ logger.py
@@ -12,15 +12,54 @@ import matplotlib.pyplot as plt
 import nidaqmx
 from nidaqmx.constants import AcquisitionType, TerminalConfiguration
 from collections import deque
+import pandas as pd
 import time
+import os
+from datetime import datetime as dt
+from datetime import timedelta as td
+import threading
+import csv
+
+
+def create_csv_file(file_name):
+    with open(file_name, 'w', newline='') as csv_file:
+        # Your CSV writing code here, for example:
+        csv_writer = csv.writer(csv_file)
+        csv_writer.writerow(['Date/Time', 'Value'])
+
+    print(f"CSV file '{file_name}' created.")
+    
+
+# def log_data(f_name):
+#     global current_data
+#     with open(f_name, 'a', newline='') as csv_file:
+#         csv_writer = csv.writer(csv_file)
+#         while True:
+#             csv_writer.writerow(current_data)
+#             time.sleep(1)
+
+def log_data(f_name, data):
+    with open(f_name, 'a', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file)
+        csv_writer.writerow(data)        
+folder_path = "csv_folder"
+if not os.path.exists(folder_path):
+    os.makedirs(folder_path)
+
+current_time = dt.now()
+second_timer = dt.now()
+time_str = current_time.strftime("%Y_%m_%d_%H_%M_%S")
+current_data = [time_str, 0]
+file_name = f"{folder_path}/data_{time_str}.csv"
+create_csv_file(file_name)
 
 # Configure NI DAQmx settings
 task = nidaqmx.Task()
 task.ai_channels.add_ai_voltage_chan("Dev1/ai0", terminal_config=TerminalConfiguration.RSE)
-task.timing.cfg_samp_clk_timing(rate=1000, sample_mode=AcquisitionType.CONTINUOUS)
+task.timing.cfg_samp_clk_timing(rate=100, sample_mode=AcquisitionType.CONTINUOUS)
 
 # Initialize variables for data storage and plotting
-history_length = 5  # seconds
+history_length = 900  # seconds
 num_samples = int(task.timing.samp_clk_rate * history_length)
 time_values = np.linspace(-history_length, 0, num_samples)
 data_buffer = deque(maxlen=num_samples)
@@ -33,14 +72,14 @@ line, = ax.plot(time_values, np.zeros(num_samples))
 ax.set_xlabel('Time (s)')
 ax.set_ylabel('Voltage (V)')
 ax.set_title('Analog Input from Dev1/ai0')
-
+#log_thread = threading.Thread(target=log_data,args=(file_name,))
+#log_thread.start()
 task.start()
 
 while True:
     try:
         new_data = task.read(number_of_samples_per_channel=50)  # Read 50 samples
         timestamp = time.time()
-
         data_buffer.extend(new_data)
         time_buffer.extend([timestamp] * len(new_data))
 
@@ -48,9 +87,26 @@ while True:
         mask = time_diff > -history_length
         line.set_xdata(-time_diff[mask])
         line.set_ydata(np.array(data_buffer)[mask])
+        value1 = data_buffer[-1]
+        print(value1)
+        now = dt.now()
+        current_data = [dt.now().strftime("%Y-%m-%d %H:%M:%S"), value1]
         ax.relim()
         ax.autoscale_view()
         plt.pause(0.01)  # Pause to allow the plot to update
+        if dt.now() - second_timer >= td(seconds=1):
+            log_data(file_name, current_data)
+            second_timer = dt.now()
+        if dt.now() - current_time >= td(hours=1):
+            current_time = dt.now()
+            time_str = current_time.strftime("%Y%m%d_%H%M%S")
+            file_name = f"{folder_path}/data_{time_str}.csv"
+            create_csv_file(file_name)
+            #log_thread.join()
+            #log_thread.start()
+        
+        
+        
     except (KeyboardInterrupt,SystemExit):
         task.stop()
         task.close()
